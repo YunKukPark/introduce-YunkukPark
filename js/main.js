14357c64e4dcd96a5a61b341aaed8dd259a2f379
YunKukPark <a01043340999@gmail.com>
2021-10-28 16:55:08 +0900

    🚧 adjusted to the window size based on the height

--- js/main.js
+++ js/main.js
@@ -118,6 +118,9 @@
         break;
       }
     }
+
+    const heightRatio = window.innerHeight / 1080;
+    sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%,-50%,0) scale(${heightRatio})`;
   }
 
   function calcValues(values, currentYOffset) {
